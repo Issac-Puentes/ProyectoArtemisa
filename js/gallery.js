f843cac329cf717be528e402e806582a5e38027b
Daay0 <danielchavez126@hotmail.com>
2021-08-10 16:01:13 -0500

    Galeria 3.0

--- js/gallery.js
+++ js/gallery.js
@@ -34,6 +34,42 @@ function showArbustus(){
   })
   } 
 
+  function showComarostaphylis(){
+
+    fetch('http://localhost:3000/Madrono/Comarostaphylis')
+    .then(Response => Response.json())
+    .then(function content(info){
+    console.log(info);
+    for(var n=0;n<info.length;n++){
+    document.getElementById('ocultar').innerHTML +=
+    '<div class="card">'+
+    '<div class="card__image-holder">'+
+    '<img class="card__image" src="https://source.unsplash.com/300x225/?kite" alt="kite" />'+
+    '</div>'+
+    '<div class="card-title">'+
+    '<a href="#" class="toggle-info btn">'+
+      '<span class="left"></span>'+
+      '<span class="right"></span>'+
+    '</a>'+
+    '<h2>'+
+    info[n].especie +
+        '<small>'+ info[n].habito +'</small>'+
+    '</h2>'+
+    '</div>'+
+    '<div class="card-flap flap1">'+
+    '<div class="card-description asd">'+
+    '<h2><small>Estados:</small></h2>'+
+      info[n].estado.replace(/;/g,', ')+
+    '</div>'+
+  '</div>'+
+  
+    '</div>';
+    }
+    }).catch(function Error(){
+    alert('Fallo *Search()*');
+    })
+    } 
+
   function pa(){
     
     var zindex = 10;
@@ -80,7 +116,5 @@ function showArbustus(){
     });
   }
 
-
   showArbustus();
-  setTimeout(p(),5);
-  
+  showComarostaphylis();  
